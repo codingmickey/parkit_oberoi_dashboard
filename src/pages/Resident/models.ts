137909b4ea5f3b213669050f3e69a6bd3ecd4d01
Kartik Jolapara <kjmickey003@gmail.com>
2023-10-12 13:24:00 +0530

    feat: add the Resident models
    Signed-off-by: Kartik Jolapara <kjmickey003@gmail.com>

--- src/pages/Resident/models.ts
+++ src/pages/Resident/models.ts
@@ -1,20 +1,27 @@
-export interface LogRecord {
-  [key: string]: string | number | null | Date | undefined;
+export interface ResidentRecord {
+  [key: string]:
+    | string
+    | number
+    | null
+    | Date
+    | boolean
+    | undefined
+    | ResidentialRegisteredVehicles
+    | ResidentialAllocatedParkings[]
+    | ParkingSpot;
   // | ParkingSpot
   // | ReadableEventInTimestampProps
   // | WhitelistReference;
-  id: number;
-  residentName: string;
-  wing: string;
-  vehicleNumber: string;
-  vehicleType: string;
-  checkInTime: Date | string;
-  checkInDate: Date | string;
-  checkOutTime: Date | string | null;
-  checkOutDate: Date | string | null;
-  duration: string;
-  checkInGate: string;
-  checkOutGate: string;
+  // residentName: string;
+  // vehicleNumber: string;
+  // vehicleType: string;
+  // checkInTime: Date | string;
+  // checkInDate: Date | string;
+  // checkOutTime: Date | string | null;
+  // checkOutDate: Date | string | null;
+  // duration: string;
+  // checkInGate: string;
+  // checkOutGate: string;
   // laneNumber: laneNumberLiteral;
   // vehicleRegistration: string | null;
   // amountCollected: number;
@@ -26,13 +33,67 @@ export interface LogRecord {
   // bookingId: number | null;
   // parkingSpot: null | ParkingSpot;
   // whitelistReference: null | WhitelistReference;
+
+  wingflat: string;
+  id: number;
+  name: string;
+  mobileNumber: string;
+  wing: string;
+  flatNumber: string;
+  ownershipType: string;
+  active: boolean;
+  deactive: boolean;
+  status: string;
+  createdAt?: string;
+  updatedAt?: string;
+  parkingSpotId: number;
+  ownedBy?: string;
+  parkingSpot: ParkingSpot;
+  residentialRegisteredVehicles: ResidentialRegisteredVehicles[];
+  residentialAllocatedParkings: ResidentialAllocatedParkings[];
 }
 export type ReadableEventInTimestampProps = {
   date: string;
   time: string;
 };
-export type WhitelistReference = { id: number; name: string };
+export type ResidentialAllocatedParkings = {
+  id: number;
+  parkingArea: null | string;
+  parkingIdentification: null | unknown;
+  ownershipType: null | string;
+  count: number;
+  active: boolean;
+  deactive: boolean;
+  createdAt: string;
+  updatedAt: string;
+  residentialId: number;
+};
+
 export type ParkingSpot = { id: number; parkingSpotName: string };
+
+export type ResidentialRegisteredVehicles = {
+  id: number;
+  vehicleRegistration: string;
+  name: string;
+  mobileNumber: null | string;
+  emailAddress: null | string;
+  expiryDate: string;
+  deactive: boolean;
+  active: boolean;
+  pushToLpu: number;
+  createdAt: string;
+  updatedAt: string;
+  residentialId: number;
+  vehicleTypeId: number;
+  vehicleColourId: null | number;
+  fuelTypeId: null | number;
+  vehicleType: {
+    id: number;
+    vehicleTypeName: string;
+  };
+};
+
+export type WhitelistReference = { id: number; name: string };
 export const laneOptionsArray = ['1', '2'] as const;
 export type laneNumberLiteral = (typeof laneOptionsArray)[number];
 export const paymentOptionsArray = ['FASTAG', 'UPI', 'DD'] as const;
@@ -50,7 +111,7 @@ export interface FilterHeaderComponentProps {
   reloadingCurrentlyOrNot: boolean;
   setFilters: React.Dispatch<React.SetStateAction<FilterProps>>;
   filters: FilterProps;
-  data: LogRecord[];
+  data: ResidentRecord[];
   setData: React.Dispatch<React.SetStateAction<LogRecord[]>>;
   together: number;
   setTogether: React.Dispatch<React.SetStateAction<number>>;
@@ -62,7 +123,7 @@ export interface TableProps {
   setReloadingCurrentlyOrNot: React.Dispatch<React.SetStateAction<boolean>>;
   setReloadTrigger: React.Dispatch<React.SetStateAction<number>>;
   filters: FilterProps;
-  data: LogRecord[];
-  setData: React.Dispatch<React.SetStateAction<LogRecord[]>>;
+  data: ResidentRecord[];
+  setData: React.Dispatch<React.SetStateAction<ResidentRecord[]>>;
   together: number;
 }
