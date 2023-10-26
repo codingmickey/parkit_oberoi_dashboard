d9f212da034c1c04d6c15bdd896fa31dc7f7633c
Kartik Jolapara <kjmickey003@gmail.com>
2023-10-26 16:03:00 +0530

    feat: Add authorization token to request headers
    Signed-off-by: Kartik Jolapara <kjmickey003@gmail.com>

--- src/api/Axios/privateInstance.tsx
+++ src/api/Axios/privateInstance.tsx
@@ -9,4 +9,10 @@ const axiosPrivateInstance = axios.create({
   }
 });
 
+axiosPrivateInstance.interceptors.request.use(function (config) {
+  const token = localStorage.getItem('token');
+  config.headers['Authorization'] = 'Bearer ' + token;
+  return config;
+});
+
 export default axiosPrivateInstance;
